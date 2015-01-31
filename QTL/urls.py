bff03ad815323c9a4e0236f43d1077e19a578323
Jiao Long <longing247@gmail.com>
2015-01-31 05:00:21 +0100

    modified:   QTL/urls.py 	modified:   qtl/templates/base.html 	modified:   qtl/urls.py

--- QTL/urls.py
+++ QTL/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from qtl import views
+from qtl.views import *
 admin.autodiscover()
 
 
@@ -9,8 +9,22 @@ urlpatterns = patterns('',
 
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^qtl/', include('qtl.urls')),
-    #url(r'^$', views.indexView,name='index'),
-
+    #url(r'^qtl/', include('qtl.urls')),
+    url(r'^$', searchGeneView,name='index'),
+    url(r'^about/$',aboutView,name='aboutView'),
+    url(r'^upload/$',uploadView,name='uploadView'),   
+    url(r'^upload/success/$',upload_success,name='uploadSuccessView'),                 
+    url(r'^gene/success/$',upload_success,name='uploadSuccessView'),  
+    url(r'^marker/success/$',upload_success,name='uploadSuccessView'), 
+    url(r'^experiment/success/$',upload_success,name='uploadSuccessView'), 
+    url(r'^parent/success/$',upload_success,name='uploadSuccessView'),
+    url(r'^gene/overlap/$',searchOverlapQTLView,name='search_overlap'), #/qtl/gene/overlap?lod_threshold=&trait=
+    url(r'^gene/$',searchGeneView,name='search_gene'),
+    url(r'^marker/$',searchMarkerView,name='search_marker'),
+    url(r'^metabolite/$',searchMetaboliteView,name='search_metabolite'),
+    url(r'^gene/chromosome/$',chromosomeView,name='chromosome'),
+    url(r'^gene/eQTLPlot/$',eQTLPlotView,name='eQTL'),
+    url(r'^chromosome/success/$',upload_success,name='success'),
+    url(r'^documentation/$',documentationView,name='documentation'),
     
 )
--- qtl/urls.py
+++ qtl/urls.py
@@ -4,28 +4,13 @@ Created on Jul 30, 2014
 @author: jiao
 '''
 from django.conf.urls import patterns,url
-from qtl import views
+from qtl.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = patterns('',
-    url(r'^$',views.searchGeneView,name='search_gene'),
-    url(r'^about/$',views.aboutView,name='aboutView'),
-    url(r'^upload/$',views.uploadView,name='uploadView'),   
-    url(r'^upload/success/$',views.upload_success,name='uploadSuccessView'),                 
-    url(r'^gene/success/$',views.upload_success,name='uploadSuccessView'),  
-    url(r'^marker/success/$',views.upload_success,name='uploadSuccessView'), 
-    url(r'^experiment/success/$',views.upload_success,name='uploadSuccessView'), 
-    url(r'^parent/success/$',views.upload_success,name='uploadSuccessView'),
-    url(r'^gene/overlap/$',views.searchOverlapQTLView,name='search_overlap'), #/qtl/gene/overlap?lod_threshold=&trait=
-    url(r'^gene/$',views.searchGeneView,name='search_gene'),
-    url(r'^marker/$',views.searchMarkerView,name='search_marker'),
-    url(r'^metabolite/$',views.searchMetaboliteView,name='search_metabolite'),
-    url(r'^gene/chromosome/$',views.chromosomeView,name='chromosome'),
-    url(r'^gene/eQTLPlot/$',views.eQTLPlotView,name='eQTL'),
-    url(r'^chromosome/success/$',views.upload_success,name='success'),
-    url(r'^documentation/$',views.documentationView,name='documentation'),
+
     
     ) 
 #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
